861dfddf10c1dd820627540cd070e0f043618795
CJRS1 <reyes.christian@pucp.pe>
2023-01-14 13:34:39 -0800

    Se añadió un elemento a la lista con getElementById y createElement

--- app.js
+++ app.js
@@ -1,23 +1,8 @@
-//Añadir texto, etiquetas en HTML con JS
+const lista = document.getElementById('lista')
+console.log(lista)
 
-console.log(document) //Estructura de HTML
-document.querySelector('H1').textContent = 'Texto desde JS' //Se cambia el texto a Texto desde JS
-console.log(document.querySelector('H1')) //Muestra lo que esta contenido en H1
+const li = document.createElement('li')
+li.textContent = 'hola'
 
-document.querySelector('h3')
-console.log(document.querySelector('h3'))
-console.log(document.querySelector('.h3-danger'))
-console.log(document.querySelector('#parrafo'))
-
-// document.getElementById('parrafo').textContent = 'hola mundo'Z
-console.log(document.getElementById('parrafo')) //hace lo mismo
-
-console.log(document.querySelectorAll('.h3-danger')) //Selecciona todos los elemntos con clase h3-danger
-console.log(document.querySelector('h3'))
-
-//Se debe guardar en una variable
-
-const parrafo = document.querySelector('#parrafo')
-// parrafo.textContent = 'Textooo desde JS'
-parrafo.innerHTML = '<b>texto con innerHTML</b>'
+lista.appendChild(li)
 
