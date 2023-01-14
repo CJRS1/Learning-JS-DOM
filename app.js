5c52b6066733ca92363633e08168e1b9f2b0cc6e
CJRS1 <reyes.christian@pucp.pe>
2023-01-14 13:45:03 -0800

    Se agregó los elementos con forEach, insertBefore y appendChild utilizando DocumentFragment

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
-//Colocar elementos en el ul (ninguna de estas formas es recomendada) genera reflow y genera multiples solicitudes
-
-//FORMA 1 (NO RECOMENDADA)
+//Forma adecuada de colocar elementos dentro de una lista en HTML con JS
 
 const arrayElement = ['primer elemento', 'segundo elemento','tercer elemento']
 
+// const fragment = document.createDocumentFragment()
+const fragment = new DocumentFragment()
+
 arrayElement.forEach(item=>{
-    lista.innerHTML += `<li>${item}</li>`
-}) 
+    const li = document.createElement('li')
+    li.textContent = item
+
+    const childNode = fragment.firstChild
+    console.log(item,childNode)
+
+    fragment.insertBefore(li,childNode)
+})
+
+lista.appendChild(fragment)
